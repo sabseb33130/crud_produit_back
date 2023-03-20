e919cb8daa7ddc35096ec8a60c7b16ef65f3ee49
jeanseb <jeansebastien.garitey@reseau.sncf.fr>
2023-03-20 13:52:56 +0100

    fix: defaut cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+var cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   const config = new DocumentBuilder()
     .setTitle('Crud Produit')
     .setDescription('Brief CRUD Produit')
